086ecd5528e58827df0c35fc734de7a876823490
bhritick <hritickbaidya1.gmail.com>
2024-05-02 22:54:06 +0530

    020520232254

---
+++ src/components/footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <>
+            <footer id="footer" className="bg-dark text-light">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            <p className="text-center py-4 mb-0">Anupam Mondal © 2024</p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
---
+++ src/pages/contact.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+
+const Contact = () => {
+	return (
+		<>
+			<Navbar />
+			<header className="bg-dark text-light">
+				<div className="container">
+					<div className="row">
+						<div className="col-12">
+							<h1 className="lead text-center fs-1 py-5 my-5">Contact Page</h1>
+						</div>
+					</div>
+				</div>
+			</header>
+			<section className="py-5">
+				<div className="container">
+					<div className="row">
+						<div className="col-md-4 col-12">
+							<div className="card my-4">
+								<img src="https://images.pexels.com/photos/707344/pexels-photo-707344.jpeg" 
+								className="card-img-top" alt="..." />
+								<div className="card-body">
+									<p className="text-center h5">
+										<Link className="nav-link text-uppercase stretched-link" to="#">Homepage</Link>
+									</p>
+								</div>
+							</div>
+						</div>
+						<div className="col-md-4 col-12">
+							<div className="card my-4">
+								<img src="https://images.pexels.com/photos/2105416/pexels-photo-2105416.jpeg" 
+								className="card-img-top" alt="..." />
+								<div className="card-body">
+									<p className="text-center h5">
+										<Link className="nav-link text-uppercase stretched-link" to="#">Contact us</Link>
+									</p>
+								</div>
+							</div>
+						</div>
+						<div className="col-md-4 col-12">
+							<div className="card my-4">
+								<img src="https://images.pexels.com/photos/2189696/pexels-photo-2189696.jpeg" 
+								className="card-img-top" alt="..." />
+								<div className="card-body">
+									<p className="text-center h5">
+										<Link className="nav-link text-uppercase stretched-link" to="#">About us</Link>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<Footer />
+		</>
+	);
+};
+
+export default Contact;
